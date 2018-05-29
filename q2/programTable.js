d3d2793884d6df206c1b770aad904a56a1b3733d
Elliot Plant <elliotaplant@gmail.com>
2018-05-29 14:53:35 -0500

    Add basic ui for frmProgram

---
+++ q2/programTable.js
@@ -0,0 +1,48 @@
+// Mock data retrieved from the PROGRAM table in the DB
+
+const programTable = [{
+  "PROGRAM_ID": 1,
+  "STATION_ID": 1,
+  "PROGRAM_NAME": "10 O''CLOCK NWS",
+  "FLIGHT_DATE": "3/11/1990"
+}, {
+  "PROGRAM_ID": 2,
+  "STATION_ID": 1,
+  "PROGRAM_NAME": "ACCESS HOLLYWD",
+  "FLIGHT_DATE": "8/25/1991"
+}, {
+  "PROGRAM_ID": 3,
+  "STATION_ID": 4,
+  "PROGRAM_NAME": "Jeop",
+  "FLIGHT_DATE": "6/30/1975"
+}, {
+  "PROGRAM_ID": 4,
+  "STATION_ID": 3,
+  "PROGRAM_NAME": "C O''BRIEN-NBC''''Program",
+  "FLIGHT_DATE": "1/1/1989"
+}, {
+  "PROGRAM_ID": 5,
+  "STATION_ID": 3,
+  "PROGRAM_NAME": "Frasier''s",
+  "FLIGHT_DATE": "11/11/1991"
+}, {
+  "PROGRAM_ID": 6,
+  "STATION_ID": 4,
+  "PROGRAM_NAME": "Barney",
+  "FLIGHT_DATE": "9/3/1966"
+}, {
+  "PROGRAM_ID": 7,
+  "STATION_ID": 1,
+  "PROGRAM_NAME": "Just Shoot me",
+  "FLIGHT_DATE": "6/6/1996"
+}, {
+  "PROGRAM_ID": 8,
+  "STATION_ID": 4,
+  "PROGRAM_NAME": "Wheel",
+  "FLIGHT_DATE": "6/30/1975"
+}, {
+  "PROGRAM_ID": 9,
+  "STATION_ID": 3,
+  "PROGRAM_NAME": "Sesame Street",
+  "FLIGHT_DATE": "5/16/1971"
+}];
