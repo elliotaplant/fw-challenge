0c682a10607caffe1a122bc36490b8508f098e49
Elliot Plant <elliotaplant@gmail.com>
2018-05-29 15:16:58 -0500

    Update ui on frmProgram

---
+++ q2/StationUpdator.js
@@ -0,0 +1,73 @@
+class StationUpdator {
+  constructor(programTable) {
+    // Stores it's own version of the programTable.
+    // Typically this would come from a network request
+    this.programTable = programTable;
+
+    // Also stores a sorted version of the program table as a lazy-computed cache
+    this.sortedProgramTable = null;
+  }
+
+  updateStation() {
+    // Get the value of the selected station from the UI
+    const stationId = this.getStationId();
+
+    // Get the earliest program for that station
+    const earliestProgram = this.earliestProgramForStation(stationId);
+
+    if (earliestProgram) {
+      // Set the UI to display the new earliest station
+      this.setProgramName(earliestProgram.PROGRAM_NAME);
+      this.setFlightDate(earliestProgram.FLIGHT_DATE);
+    } else {
+      // We were unable to find the earliest program for this station, so display an error
+      this.setProgramName('Unable to find earliest program');
+      this.setFlightDate('');
+    }
+  }
+
+  // Gets the selected station ID from the UI
+  getStationId() {
+    return parseInt(document.getElementById('station-select').value);
+  }
+
+  // Sets the program name in the UI
+  setProgramName(name) {
+    document.getElementById('program-name').textContent = name;
+  }
+
+  // Sets the program flight date in the UI
+  setFlightDate(date) {
+    document.getElementById('flight-date').textContent = date;
+  }
+
+  // Finds the earliest program for a station
+  earliestProgramForStation(stationId) {
+    // Lazily creates the sortedProgramTable only when it is required to improve page load time
+    if (!this.sortedProgramTable) {
+      this.sortedProgramTable = this.programTable.sort(this.sortProgramsByStartDateAlpha);
+    }
+
+    // Finds the first program that matches the station we're looking for, or null if it can't find it
+    return this.sortedProgramTable.find(program => program.STATION_ID === stationId);
+  }
+
+  // Sorts programs first chronologically by FLIGHT_DATE then alphabetically by PROGRAM_NAME
+  sortProgramsByStartDateAlpha(programA, programB) {
+    const startDateA = new Date(programA.FLIGHT_DATE);
+    const startDateB = new Date(programB.FLIGHT_DATE);
+
+    if (startDateA > startDateB) {
+      return 1;
+    } else if (startDateA < startDateB) {
+      return -1;
+    } else {
+      if (programA.PROGRAM_NAME > programB.PROGRAM_NAME) {
+        return 1;
+      } else if (programA.PROGRAM_NAME < programB.PROGRAM_NAME) {
+        return -1;
+      }
+    }
+    return 0;
+  }
+}
--- q2/frmProgram.js
+++ q2/frmProgram.js
@@ -1,15 +1,1 @@
-function updateStation() {
-  // Find the station selctor on the DOM
-  const stationSelect = document.getElementById('station-select');
-
-  // Get the value of the selected station as an INT to match DB
-  const stationId = parseInt(stationSelect.value);
-
-  // Find the programs on that station from the programTable. Note that `programTable` is in this
-  // scope because of this line in frmProgram.html:
-  // <script type="text/javascript" src="programTable.js"></script>
-  const stationsPrograms = programTable.filter(program => program.STATION_ID === stationId);
-
-  // Sort the stations programs first by 
-  console.log('stationsPrograms', stationsPrograms);
-}
+const stationUpdator = new StationUpdator(programTable);
